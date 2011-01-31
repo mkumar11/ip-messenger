21ce2d6516698c5efb0882a4cc6e15575646818c
yaragallamurali <yaragallamurali@gmail.com>
2011-01-31 16:32:46 +0000

    This is version1.0.  This is stabilized.

---
+++ ip-messenger-1.0/src/com/ymd/gui/ChatGui.java
@@ -0,0 +1,90 @@
+package com.ymd.gui;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+
+import com.ymd.gui.listner.ChatGUIUAListener;
+
+/**
+ * This is the GUI class for chat window.
+ * 
+ * @author yaragalla Muralidhar
+ *
+ */
+public class ChatGui extends JFrame {
+	
+	private static final long serialVersionUID = 4942132120249803370L;
+	
+	private JTextPane ma;
+	private JTextArea ua;
+	
+	
+	private final OutputStream out;
+	
+	/**
+	 * Constructor which configures the main properties 
+	 * and creates the required GUI.
+	 * @param title
+	 * @param out
+	 */
+	public ChatGui(String title,final OutputStream out){
+		super(title);
+		this.out=out;
+		JDesktopPane dp=new JDesktopPane();
+		dp.setLayout(null);
+		this.setContentPane(dp);
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				try{
+					out.write(-1);
+				}catch(IOException ioe){
+					System.out.println(ioe);
+				}
+			}
+		});
+		
+		ma=new JTextPane();
+		ma.setEditable(false);
+		JScrollPane jspma=new JScrollPane(ma);		
+		jspma.setBounds(0, 0, 250, 300);
+		dp.add(jspma);
+		
+		ua=new JTextArea();
+		ua.setToolTipText("Enter your chat message here...");
+		ua.setLineWrap(true);
+		ua.setWrapStyleWord(true);
+		ua.addKeyListener(new ChatGUIUAListener(this));
+		JScrollPane jspua=new JScrollPane(ua);		
+		jspua.setBounds(0, 300, 250, 100);		
+		dp.add(jspua);		
+		
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		this.setSize(260, 450);
+		this.setVisible(true);
+	}
+	
+	
+	
+
+	public JTextPane getMa() {
+		return ma;
+	}
+
+	public JTextArea getUa() {
+		return ua;
+	}	
+
+	public OutputStream getOut() {
+		return out;
+	}
+		
+}
---
+++ ip-messenger-1.0/src/com/ymd/main/IPMessenger.java
@@ -0,0 +1,148 @@
+package com.ymd.main;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import com.ymd.gui.MainGui;
+import com.ymd.net.ChatServer;
+import com.ymd.net.Packets;
+import com.ymd.util.Util;
+
+
+/**
+ * This is the main class for IPMessenger.
+ * 
+ * @author Muralidhar Yaragalla
+ *
+ */
+public class IPMessenger {
+
+	/**
+	 * This is the Main method.
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		try{
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			Map<String,DefaultMutableTreeNode> nodeMap=new HashMap<String,DefaultMutableTreeNode>();
+			Thread server=new Thread(new ChatServer());
+			server.start();
+			MulticastSocket multicastSoc=new MulticastSocket(1988); 
+			InetAddress group = InetAddress.getByName("225.5.6.4");
+			multicastSoc.joinGroup(group);
+			MainGui gi=new MainGui("IPMessenger",multicastSoc,group);
+			JTree mainTree=gi.getMainTree();
+			DefaultTreeModel treeModel=(DefaultTreeModel)mainTree.getModel();
+			DefaultMutableTreeNode top=gi.getTop();
+			boolean firsttime=true;			
+			
+			Packets.sendIntialPacket(multicastSoc,group);
+			while(true){
+				byte[] buf = new byte[68];
+				 DatagramPacket recv = new DatagramPacket(buf, buf.length);
+				 multicastSoc.receive(recv);
+				 StringBuffer temp=new StringBuffer();
+				 for(int i=0;i<buf.length;i++){
+					 char chr=(char)buf[i];
+					 temp.append(chr);					
+				 }
+				 String pack=temp.toString();
+				 String[] tok=pack.split("&");				 
+				 if(tok[0].equalsIgnoreCase("hello")){					 
+					 String ip=Util.removePadding(tok[1]);				 
+					 if(!Util.localHost(ip)){
+						 Packets.sendHandShakePacket(multicastSoc,group);
+						 if(!nodeMap.containsKey(ip)){
+							 String name="HostName: "+Util.removePadding(tok[2]);
+							 String user="UserName: "+Util.removePadding(tok[3]);						 
+							 int existingNodes=top.getChildCount();
+
+							 DefaultMutableTreeNode ipNode=new DefaultMutableTreeNode(ip);							 
+							 DefaultMutableTreeNode nameNode=new DefaultMutableTreeNode(name);
+							 DefaultMutableTreeNode userNode=new DefaultMutableTreeNode(user);							 
+							 ipNode.add(nameNode);
+							 ipNode.add(userNode);
+							 
+							 nodeMap.put(ip, ipNode);
+							 
+							 top.add(ipNode);
+							 if(firsttime){
+								 mainTree.expandRow(0);
+								 firsttime=false;
+							 }else{
+								 
+								 int index[]={existingNodes};
+								 treeModel.nodesWereInserted(top,index );
+							 }
+						 }
+						
+					 
+					 }
+				 }
+				 
+				 if(tok[0].equalsIgnoreCase("hands")){
+					 String ip=Util.removePadding(tok[1]);
+					 if(!Util.localHost(ip)){
+						 if(!nodeMap.containsKey(ip)){
+							 String name="HostName: "+Util.removePadding(tok[2]);
+							 String user="UserName: "+Util.removePadding(tok[3]);						 
+							 int existingNodes=top.getChildCount();
+							 
+							 DefaultMutableTreeNode ipNode=new DefaultMutableTreeNode(ip);							 
+							 DefaultMutableTreeNode nameNode=new DefaultMutableTreeNode(name);
+							 DefaultMutableTreeNode userNode=new DefaultMutableTreeNode(user);							 
+							 ipNode.add(nameNode);
+							 ipNode.add(userNode);
+							 
+							 nodeMap.put(ip, ipNode);
+							 
+							 top.add(ipNode);
+							 if(firsttime){
+								 mainTree.expandRow(0);
+								 firsttime=false;
+							 }else{
+								 int index[]={existingNodes};
+								 treeModel.nodesWereInserted(top,index );
+							 }
+						 }
+					 }
+				 }
+				 
+				 if(tok[0].equalsIgnoreCase("goodb")){
+					 String ip=Util.removePadding(tok[1]);
+					 if(!Util.localHost(ip)){
+						 DefaultMutableTreeNode node=nodeMap.get(ip); 
+						 if(node != null){
+							 int index=top.getIndex(node);
+							 int indexArr[]={index};
+							 Object removedChildren[]={node};
+							 top.remove(node);
+							 treeModel.nodesWereRemoved(top, indexArr, removedChildren);
+							 nodeMap.remove(ip);
+						 }
+					 }
+				 }
+			}
+		}catch(IOException ioe){
+			System.out.println(ioe);
+		}catch(UnsupportedLookAndFeelException ulfe){
+			System.out.println(ulfe);
+		}catch(IllegalAccessException ie){
+			System.out.println(ie);
+		}catch(InstantiationException instEx){
+			System.out.println(instEx);
+		}catch(ClassNotFoundException cnfe){
+			System.out.println(cnfe);
+		}
+	}
+}
